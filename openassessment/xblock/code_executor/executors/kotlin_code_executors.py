1dd30e4a818e295cfcfcf5ed61bf5251b2a2efdc
Abubakar Siddique <abubakar.siddique@arbisoft.com>
2025-05-21 11:59:23 +0500

    feat: add SQLite support for Swift and Kotlin, LT-1619

--- openassessment/xblock/code_executor/executors/kotlin_code_executors.py
+++ openassessment/xblock/code_executor/executors/kotlin_code_executors.py
@@ -1,10 +1,9 @@
 from .mixins import CompiledLanguageExecutorMixin
-
 from ..interface import CodeExecutor
 
 
 class KotlinCodeExecutor(CompiledLanguageExecutorMixin, CodeExecutor):
-    docker_image = 'litmustest/code-executor-kotlin:1.9.23'
+    docker_image = 'litmustest/code-executor-kotlin-sqlite:1.9.23'
     language = 'kotlin'
     version = '1.9.23'
     display_name = 'Kotlin 1.9.23'
@@ -13,6 +12,33 @@ class KotlinCodeExecutor(CompiledLanguageExecutorMixin, CodeExecutor):
 
     SOURCE_FILE_NAME_TEMPLATE = 'Main.kt'
     EXECUTABLE_FILE_NAME_TEMPLATE = 'MainKt'
-    COMPILE_COMMAND_TEMPLATE = 'kotlinc -Xmx512m -Xms128m {source_file}'
-    RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'kotlin $JAVA_OPTS {executable_file}'
-    RUN_COMMAND_FILE_INPUT_TEMPLATE = 'kotlin $JAVA_OPTS {executable_file} {input_file}'
+
+    # Compile to jar
+    COMPILE_COMMAND_TEMPLATE = (
+        'kotlinc -J-Xmx512m -J-Xms256m \
+            -J-Dorg.sqlite.tmpdir=/sandbox/tmp \
+                  -cp "/app/lib/*" {source_file} \
+                    -include-runtime -d /sandbox/app.jar'
+    )
+
+    # Run with stdin
+    RUN_COMMAND_STDIN_INPUT_TEMPLATE = (
+        'java -Xms256m -Xmx1024m \
+            -Dorg.sqlite.tmpdir=/sandbox/tmp \
+            -Dorg.sqlite.lib.path=/sandbox/sqlite-lib \
+                -Dsqlite.purejava=true \
+                  -Djava.io.tmpdir=/sandbox/tmp \
+                    -cp "/app/lib/*:/sandbox/app.jar" \
+                      {executable_file}'
+    )
+
+    # Run with file input
+    RUN_COMMAND_FILE_INPUT_TEMPLATE = (
+        'java -Xms256m -Xmx1024m \
+            -Dorg.sqlite.tmpdir=/sandbox/tmp \
+            -Dorg.sqlite.lib.path=/sandbox/sqlite-lib \
+                -Dsqlite.purejava=true \
+                  -Djava.io.tmpdir=/sandbox/tmp \
+                    -cp "/app/lib/*:/sandbox/app.jar" \
+                      {executable_file} {input_file}'
+    )
