552e6a71fe7bf0e942f2b40842d99f520e420849
Mujtaba-Akbar <mujtaba.akbar@arbisoft.com>
2025-09-24 04:55:09 +0500

    Dotnet executor class added

---
+++ openassessment/xblock/code_executor/executors/dotnet_code_executors.py
@@ -0,0 +1,16 @@
+from .mixins import ScriptedLanguageExecutorMixin
+
+from ..interface import CodeExecutor
+
+
+class DotnetCodeExecutor(ScriptedLanguageExecutorMixin, CodeExecutor):
+    docker_image = 'litmustest/code-executor-dotnet-sqlite:8.0'
+    language = 'dotnet'
+    version = '8.0'
+    display_name = 'Dotnet 8.0'
+
+    id = CodeExecutor.create_id('dotnet', '8.0')
+
+    SOURCE_FILE_NAME_TEMPLATE = 'Application/{name}.cs'
+    RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'dotnet run {source_file}'
+    RUN_COMMAND_FILE_INPUT_TEMPLATE = 'dotnet run {source_file} {input_file}'
