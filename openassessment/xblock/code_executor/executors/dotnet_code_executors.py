5d78ab9f74bfe017c6017a9abc801174f7a103ac
Mujtaba-Akbar <mujtaba.akbar@arbisoft.com>
2025-09-24 23:39:54 +0500

    Compile run fix

--- openassessment/xblock/code_executor/executors/dotnet_code_executors.py
+++ openassessment/xblock/code_executor/executors/dotnet_code_executors.py
@@ -1,9 +1,9 @@
-from .mixins import ScriptedLanguageExecutorMixin
+from .mixins import CompiledLanguageExecutorMixin
 
 from ..interface import CodeExecutor
 
 
-class DotnetCodeExecutor(ScriptedLanguageExecutorMixin, CodeExecutor):
+class DotnetCodeExecutor(CompiledLanguageExecutorMixin, CodeExecutor):
     docker_image = 'litmustest/code-executor-dotnet-sqlite:8.0'
     language = 'dotnet'
     version = '8.0'
@@ -11,6 +11,20 @@ class DotnetCodeExecutor(ScriptedLanguageExecutorMixin, CodeExecutor):
 
     id = CodeExecutor.create_id('dotnet', '8.0')
 
-    SOURCE_FILE_NAME_TEMPLATE = 'Application/{name}.cs'
-    RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'dotnet run {source_file} --project Application'
-    RUN_COMMAND_FILE_INPUT_TEMPLATE = 'dotnet run {source_file} {input_file} --project Application'
+    SOURCE_FILE_NAME_TEMPLATE = 'Program.cs'
+    EXECUTABLE_FILE_NAME_TEMPLATE = 'Application'  # Project name for dotnet run
+    
+    # Create a temporary project and copy the source code to Program.cs
+    COMPILE_COMMAND_TEMPLATE = (
+        'mkdir -p temp_project && '
+        'cp {source_file} temp_project/Program.cs && '
+        'cd temp_project && '
+        'dotnet new console --force && '
+        'dotnet add package Microsoft.Data.Sqlite --version 8.0.0 && '
+        'cp Program.cs.bak Program.cs 2>/dev/null || cp Program.cs Program.cs.bak && '
+        'cp ../Program.cs Program.cs && '
+        'dotnet build -o ../output'
+    )
+    
+    RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'cd output && dotnet *.dll'
+    RUN_COMMAND_FILE_INPUT_TEMPLATE = 'cd output && dotnet *.dll {input_file}'
