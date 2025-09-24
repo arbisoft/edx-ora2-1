8a4fb773e1d46a9cb34bc1c7eb24f9721d997f18
Mujtaba-Akbar <mujtaba.akbar@arbisoft.com>
2025-09-24 22:32:07 +0500

    update cli command for dotnet run

--- openassessment/xblock/code_executor/executors/dotnet_code_executors.py
+++ openassessment/xblock/code_executor/executors/dotnet_code_executors.py
@@ -12,5 +12,5 @@ class DotnetCodeExecutor(ScriptedLanguageExecutorMixin, CodeExecutor):
     id = CodeExecutor.create_id('dotnet', '8.0')
 
     SOURCE_FILE_NAME_TEMPLATE = 'Application/{name}.cs'
-    RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'dotnet run {source_file}'
-    RUN_COMMAND_FILE_INPUT_TEMPLATE = 'dotnet run {source_file} {input_file}'
+    RUN_COMMAND_STDIN_INPUT_TEMPLATE = 'dotnet run {source_file} --project Application'
+    RUN_COMMAND_FILE_INPUT_TEMPLATE = 'dotnet run {source_file} {input_file} --project Application'
